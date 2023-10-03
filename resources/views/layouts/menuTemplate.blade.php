f51eb527b0f0730069c7853fd7f1007a6a15aac5
AsukaaNao <christiantanjung27@gmail.com>
2023-10-03 10:19:55 +0700

    ada

--- resources/views/layouts/menuTemplate.blade.php
+++ resources/views/layouts/menuTemplate.blade.php
@@ -1,5 +1,3 @@
-@extends('layouts_template')
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -50,12 +48,7 @@
             </div>
         </div>
     </nav>
-    <div class="container mt-4">
-        <h1>My Company</h1>
-        <p>All about my Company</p>
-        <p>Company Name : {{ $companyname }}</p>
-        <p>Company Desc : {{ $companydesc }}</p>
-    </div>
+    @yield('layout_content')
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </body>
